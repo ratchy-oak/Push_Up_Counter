4550e5c80a76dbac584bcd15f3217c25bec88e19
Ratchakrit Mojomsin <65458784+ratchy-oak@users.noreply.github.com>
2023-11-15 06:00:40 +0700

    Add files via upload

--- Counter.ino
+++ Counter.ino
@@ -7,11 +7,32 @@
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2SNoDAC.h"
 #include "whistle.h"
+#include "notcount.h"
+#include "one.h"
+#include "two.h"
+#include "three.h"
+#include "four.h"
+#include "five.h"
+#include "six.h"
+#include "seven.h"
+#include "eight.h"
+#include "nine.h"
+#include "ten.h"
+#include "eleven.h"
+#include "twelve.h"
+#include "thirteen.h"
+#include "fourteen.h"
+#include "fifteen.h"
+#include "sixteen.h"
+#include "seventeen.h"
+#include "eighteen.h"
+#include "nineteen.h"
+#include "twenty.h"
 
 // LCD
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#define time 120000
+#define time 15000
 
 // Ultrasonic
 SoftwareSerial mySerial(12, 14);
@@ -72,7 +93,8 @@ void setup() {
 
     // Ultrasonic
     mySerial.flush();
-    mySerial.write(0X55);          
+    mySerial.write(0X55);
+    delay(50);
     if (mySerial.available() >= 2) {
       HighByte = mySerial.read();
       LowByte  = mySerial.read();
@@ -99,6 +121,20 @@ void setup() {
           //notcount
           else if (Dis > 45) {
             checkstart = false;
+            audioLogger = &Serial;
+            file = new AudioFileSourcePROGMEM( notcount, sizeof(notcount) );
+            out = new AudioOutputI2SNoDAC();
+            wav = new AudioGeneratorWAV();
+            wav->begin(file, out);
+            do {
+              if (wav->isRunning()) {
+                if (!wav->loop()) {
+                  wav->stop();
+                  break;
+                } 
+              } 
+            }   
+            while (1);
           }
         }
 
@@ -111,6 +147,21 @@ void setup() {
           else if (Dis > 45) {
             checkstart = false;
             check10 = false;
+            checkstart = false;
+            audioLogger = &Serial;
+            file = new AudioFileSourcePROGMEM( notcount, sizeof(notcount) );
+            out = new AudioOutputI2SNoDAC();
+            wav = new AudioGeneratorWAV();
+            wav->begin(file, out);
+            do {
+              if (wav->isRunning()) {
+                if (!wav->loop()) {
+                  wav->stop();
+                  break;
+                } 
+              } 
+            }   
+            while (1);
           }
         }
       }
@@ -125,15 +176,323 @@ void setup() {
   while ((millis() - 1000) - prMillis < time);
 
   // Audio
-  file = new AudioFileSourcePROGMEM(whistle, sizeof(whistle) );
+  audioLogger = &Serial;
+  file = new AudioFileSourcePROGMEM( whistle, sizeof(whistle) );
   out = new AudioOutputI2SNoDAC();
   wav = new AudioGeneratorWAV();
   wav->begin(file, out);
+  do {
+    if (wav->isRunning()) {
+      if (!wav->loop()) {
+        wav->stop();
+        break;
+      } 
+    } 
+  }   
+  while (1);
+
+  if (Count == 1) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( one, sizeof(one) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 2) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( two, sizeof(two) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 3) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( three, sizeof(three) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 4) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( four, sizeof(four) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 5) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( five, sizeof(five) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 6) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( six, sizeof(six) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 7) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( seven, sizeof(seven) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 8) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( eight, sizeof(eight) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 9) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( nine, sizeof(nine) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 10) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( ten, sizeof(ten) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 11) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( eleven, sizeof(eleven) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 12) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( twelve, sizeof(twelve) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 13) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( thirteen, sizeof(thirteen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 14) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( fourteen, sizeof(fourteen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 15) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( fifteen, sizeof(fifteen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 16) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( sixteen, sizeof(sixteen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 17) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( seventeen, sizeof(seventeen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 18) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( eighteen, sizeof(eighteen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 19) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( nineteen, sizeof(nineteen) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  } else if (Count == 20) {
+    audioLogger = &Serial;
+    file = new AudioFileSourcePROGMEM( twenty, sizeof(twenty) );
+    out = new AudioOutputI2SNoDAC();
+    wav = new AudioGeneratorWAV();
+    wav->begin(file, out);
+    do {
+      if (wav->isRunning()) {
+        if (!wav->loop()) {
+          wav->stop();
+          break;
+        } 
+      } 
+    }   
+    while (1);
+  }
 }
 
 void loop() {
-  // Audio
-  if (wav->isRunning()) {
-    if (!wav->loop()) wav->stop();
-  }
 }
